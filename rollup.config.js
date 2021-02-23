18e4d7fb52a775c06eca415bc13b8a2a4ffe7e81
shoonia <zaytsev126@gmail.com>
2021-02-23 21:36:27 +0200

    Add core library to package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import copy from 'rollup-plugin-copy';
 
 const babelPlugin = getBabelOutputPlugin({
   presets: [
@@ -15,30 +16,52 @@ const babelPlugin = getBabelOutputPlugin({
   ],
 });
 
-export default {
-  input: './src/index.js',
-  output: [
-    {
-      file: './lib/esm.js',
-      format: 'esm',
-    },
-    {
-      file: './lib/es5.esm.js',
-      format: 'esm',
-      plugins: [
-        babelPlugin,
-      ],
-    },
-    {
-      file: './lib/es5.cjs.js',
-      format: 'cjs',
-      plugins: [
-        babelPlugin,
-      ],
-    },
-  ],
-  plugins: [
-    commonjs(),
-    nodeResolve(),
-  ],
-};
+export default [
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: './lib/esm.js',
+        format: 'esm',
+      },
+      {
+        file: './lib/es5.esm.js',
+        format: 'esm',
+        plugins: [
+          babelPlugin,
+        ],
+      },
+      {
+        file: './lib/es5.cjs.js',
+        format: 'cjs',
+        plugins: [
+          babelPlugin,
+        ],
+      },
+    ],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+    ],
+  },
+  {
+    input: './node_modules/storeon/index.js',
+    output: [
+      {
+        file: './core/index.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      babelPlugin,
+      copy({
+        targets: [
+          {
+            src: './node_modules/storeon/index.d.ts',
+            dest: 'core',
+          },
+        ],
+      }),
+    ],
+  },
+];
