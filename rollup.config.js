0e56235cfa22efbf58a6126f2f634be9bc981047
shoonia <zaytsev126@gmail.com>
2023-01-02 14:46:14 +0200

    Add new build script

--- rollup.config.js
+++ rollup.config.js
@@ -4,18 +4,39 @@ if (existsSync('./lib')) {
   rmSync('./lib', { recursive: true });
 }
 
-export default {
-  input: './src/index.js',
-  output: [
-    {
-      file: './lib/index.js',
-      format: 'es',
-    },
-    {
-      file: './lib/index.cjs',
-      format: 'cjs',
-      interop: false,
-      esModule: false,
-    },
-  ],
-};
+if (existsSync('./legacy')) {
+  rmSync('./legacy', { recursive: true });
+}
+
+export default [
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: './lib/index.js',
+        format: 'es',
+      },
+      {
+        file: './lib/index.cjs',
+        format: 'cjs',
+        interop: false,
+        esModule: false,
+      },
+    ],
+  },
+  {
+    input: './src/legacy.js',
+    output: [
+      {
+        file: './legacy/index.js',
+        format: 'es',
+      },
+      {
+        file: './legacy/index.cjs',
+        format: 'cjs',
+        interop: false,
+        esModule: false,
+      },
+    ],
+  }
+];
