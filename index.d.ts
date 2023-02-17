262034ddd05124366a148e298cf6554abda321bd
shoonia <zaytsev126@gmail.com>
2023-02-17 17:53:03 +0200

    Add type

--- index.d.ts
+++ index.d.ts
@@ -98,7 +98,7 @@ export namespace createStoreon {
     state: State extends object ? Readonly<State> : State,
     data: (Events & StoreonEvents<State, Events>)[Event],
     store: StoreonStore<State, Events>
-  ) => Partial<State> | Promise<void> | null | void
+  ) => Partial<State> | Promise<void> | null | void | false
 
   export interface DispatchableEvents<State> {
     '@init': never
