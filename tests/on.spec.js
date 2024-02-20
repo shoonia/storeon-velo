6a12063d5858c2a443a93a19d1f8976ae6523b8b
shoonia <zaytsev126@gmail.com>
2024-02-20 21:42:17 +0200

    Add test

---
+++ tests/on.spec.js
@@ -0,0 +1,28 @@
+import { randomUUID } from 'node:crypto';
+import { jest } from '@jest/globals';
+import { createStoreon } from '..';
+
+describe('state.on()', () => {
+  it('should unsubscribe event listener', () => {
+    expect.hasAssertions();
+
+    const event = randomUUID();
+    const spy = jest.fn();
+
+    /** @type {Function} */
+    let off;
+
+    const { dispatch } = createStoreon([
+      (store) => {
+        off = store.on(event, spy);
+      },
+    ]);
+
+    dispatch(event, '1');
+    off();
+    dispatch(event, '2');
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({}, '1');
+  });
+});
